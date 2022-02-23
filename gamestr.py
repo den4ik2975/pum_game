6b54c6763907c5ecc475f5fdc1b84bc4a900cc02
den4ik2975 <mai.avramenko.denis@gmail.com>
2022-02-23 17:34:57 +0300

    beta2

--- gamestr.py
+++ gamestr.py
@@ -34,50 +34,28 @@ class Game:
         self.max_fighter_price = self.market_levels[str(self.market_level)][3]
 
 
-    def sorting_orders(self, current_ind, hlp):
-        last = self.sorted_players_raw_orders[current_ind:hlp]
-        chance = []
-        while len(last) != 0:
-            for order in last:
-                if order[1][0] <= self.material_bank:
-                    chance.append(order)
-            if len(chance) == 0:
-                break
-            lucky_man = random.choice(chance)
-            self.material_bank -= lucky_man[1][0]
-            self.players_profiles[lucky_man[0]].cash -= lucky_man[1][1] * lucky_man[1][0]
-            self.players_profiles[lucky_man[0]].material_num += lucky_man[1][0]
-            del chance[chance.index(lucky_man)]
-            last = chance[:]
-            chance.clear()
-
     def raw_handling(self):
-        self.sorted_players_raw_orders = sorted(self.players_raw_orders.items(), key=lambda item: item[1][1], reverse=True)
-        print(self.sorted_players_raw_orders)
-
-        counter = 0
-        current_ind = 0
-        hlp_counter = 0
-        hlp = 0
+        self.sorted_players_raw_orders = sorted(self.players_raw_orders.items(), key=lambda item: (item[1][1], item[1][0]), reverse=True)
+        self.spread_players_raw_orders = []
+        cur_price = self.sorted_players_raw_orders[0][1][1]
+        hlp = []
         for order in self.sorted_players_raw_orders:
-            if counter == 0:
-                current_price = order[1][1]
-            if order[1][1] == current_price:
-                counter += order[1][0]
-                hlp += 1
-            elif order[1][1] != current_price:
-                if self.material_bank >= counter:
-                    self.material_bank -= counter
-                    counter = 0
-                    current_ind = hlp
-                    hlp_counter = hlp
+            if order[1][1] == cur_price:
+                hlp.append(order)
+            elif order[1][1] != cur_price:
+                self.spread_players_raw_orders += [hlp[:]]
+                hlp.clear()
+                hlp.append(order)
+                cur_price = order[1][1]
+        self.spread_players_raw_orders += [hlp[:]]
+        for order_pack in self.spread_players_raw_orders:
+            for order in order_pack:
+                if order[1][0] <= self.material_bank:
+                    self.material_bank -= order[1][0]
+                    self.players_profiles[order[0]].cash -= order[1][0] * order[1][1]
+                    self.players_profiles[order[0]].material_num += order[1][0]
                 else:
-                    self.sorting_orders(current_ind, hlp)
-                    hlp_counter = len(self.sorted_players_raw_orders)
                     break
-        self.sorting_orders(hlp_counter, hlp)
-        for order in self.sorted_players_raw_orders[:current_ind]:
-            self.players_profiles[order[0]].cash -= order[1][1] * order[1][0]
-            self.players_profiles[order[0]].material_num += order[1][0]
+
         for i in self.players_profiles.values():
             print(i.cash, i.material_num)
